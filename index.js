03992002cd0743342375af3586a6b6dfdae17a30
Oscar Oceguera bibriesca <oscaroceguera@gmail.com>
2024-11-07 18:50:05 -0700

    scroll animation

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "print-media-queries")));
 app.use(express.static(path.join(__dirname, "text-gradiant")));
+app.use(express.static(path.join(__dirname, "scroll-timeline-cube")));
 
 app.get("/text-gradiant", async (req, res) => {
   res.sendFile(path.join(__dirname, "text-gradiant", "index.html"));
 });
 app.get("/", async (req, res) => {
   res.sendFile(path.join(__dirname, "print-media-queries", "index.html"));
+});
+app.get("/scroll-timeline-cube", async (req, res) => {
+  res.sendFile(path.join(__dirname, "scroll-timeline-cube", "index.html"));
 });
 
 app.listen(8080, () => {
